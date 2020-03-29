7036322ce2528b43394e6f9288de498a0ed5edbe
skuttenkuler <sam.kuttenk@gmail.com>
2020-03-28 23:08:47 -0700

    initial, src with main.cpp and build folder

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+int main() {
+    // set gmae screen and frames
+    constexpr std::size_t kFramesPerSecond{60};
+    constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
+    constexpr std::size_t kScreenWidth{640};
+    constexpr std::size_t kScreenHeight{640};
+    constexpr std::size_t kGridWidth{32};
+    constexpr std::size_t kGridHeight{32};
+
+
+    //renderer
+    //controller
+    //game
+
+    //run game
+
+    std::cout << "Game ended successfully.\n";
+    std::cout << "Score: " << "Score";
+    std::cout << "Snake Size: " << "Snake Size";
+    return 0
+}
